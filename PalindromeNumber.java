16d357f2b540bcfe4694c1cb7ea561d8e00ba1d0
navyasri <navyasridumpala@gmail.com>
2024-12-11 02:07:42 +0530

    PalindromeNumber.java contents with navyasri

---
+++ PalindromeNumber.java
@@ -0,0 +1,20 @@
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        int number = 1221; // Example number
+        int reversedNumber = 0;
+        int originalNumber = number;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+
+        if (originalNumber == reversedNumber) {
+            System.out.println(originalNumber + " is a palindrome.");
+        } else {
+            System.out.println(originalNumber + " is not a palindrome.");
+        }
+    }
+}
+
